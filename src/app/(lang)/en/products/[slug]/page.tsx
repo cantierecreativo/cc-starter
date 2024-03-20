36149ecbd62690ad67aea06dcebe4db02fc89a0f
Alberto De Stefani <orionlight231@gmail.com>
2024-03-20 16:51:55 +0100

    wip algolia

--- src/app/(lang)/en/products/[slug]/page.tsx
+++ src/app/(lang)/en/products/[slug]/page.tsx
@@ -1,10 +1,15 @@
 import fetchDato from "@/lib/fetchDato";
 import { draftMode } from "next/headers";
+<<<<<<< HEAD
 import { PageDocument, SiteLocale } from "@/graphql/generated";
 import { notFound } from "next/navigation";
 import GenericPage from "@/components/Templates/GenericPage";
 import getSeoMeta from "@/lib/seoUtils";
 import config from "@/data/config";
+=======
+import { PostDocument, PageDocument, SiteLocale } from "@/graphql/generated";
+import { notFound } from "next/navigation";
+>>>>>>> facc883 (wip algolia)
 
 type Params = {
   params: {
@@ -12,6 +17,7 @@ type Params = {
   };
 };
 
+<<<<<<< HEAD
 const locale = "en";
 const siteLocale = locale as SiteLocale;
 const defaultLocale = config.defaultLocale as SiteLocale;
@@ -46,4 +52,13 @@ export default async function Page({ params: { slug } }: Params) {
   if (!data?.page) notFound();
   console.log(data, slug, locale);
   return <GenericPage data={data} locale={siteLocale} />;
+=======
+const locale = 'en';
+const siteLocale = locale as SiteLocale;
+
+export default async function EmptyPage({ params: { slug } }: Params) {
+  const { isEnabled } = draftMode();
+
+  return <div>SLUG = {slug}</div>;
+>>>>>>> facc883 (wip algolia)
 }
