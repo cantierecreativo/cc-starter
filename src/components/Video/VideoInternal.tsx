817002598b576c90437731929a8e6be30cb60d40
Alessandro Donnini <info@alessandrodonnini.com>
2025-04-18 15:31:37 +0200

    Restore video internal

--- src/components/Video/VideoInternal.tsx
+++ src/components/Video/VideoInternal.tsx
@@ -1,19 +1,30 @@
 "use client";
+
 import { VideoFileField } from "@/graphql/generated";
-import Image from "next/image";
-import Video from "next-video";
+
+import "@vidstack/react/player/styles/base.css";
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
+import { MediaPlayer, MediaProvider, Poster } from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
 
 const VideoInternal = ({ video }: { video: VideoFileField }) => {
   return (
-    <Video src={video.video.streamingUrl}>
-      <Image
-        slot="poster"
+    <MediaPlayer title={video.video.title} src={video.video.streamingUrl}>
+      <MediaProvider />
+      <Poster
+        className="vds-poster"
         src={video.video.thumbnailUrl}
         alt={video.title}
-        width={600}
-        height={400}
       />
-    </Video>
+      <DefaultVideoLayout
+        thumbnails={video.video.thumbnailUrl}
+        icons={defaultLayoutIcons}
+      />
+    </MediaPlayer>
   );
 };
 export default VideoInternal;
