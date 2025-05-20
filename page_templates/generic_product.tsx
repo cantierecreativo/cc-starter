f65e43236d6f875222637f3f0c539b8273fb963a
Alessandro Donnini <info@alessandrodonnini.com>
2025-05-20 09:48:33 +0200

    Fix product page

--- page_templates/generic_product.tsx
+++ page_templates/generic_product.tsx
@@ -2,7 +2,7 @@ import fetchDato from "@/lib/fetchDato";
 import { draftMode } from "next/headers";
 import { ProductDocument, SiteLocale } from "@/graphql/generated";
 import { notFound } from "next/navigation";
-import GenericPage from "@/components/Templates/GenericPage";
+import ProductPage from "@/components/Templates/ProductPage";
 import getSeoMeta from "@/lib/seoUtils";
 import config from "@/data/config";
 import { pickHrefs } from "@/lib/pickPageData";
@@ -47,8 +47,7 @@ export default async function Page() {
 
   return (
     <Wrapper hrefs={hrefs} locale={locale}>
-      {JSON.stringify(data.product)}
-      {/* <GenericPage data={data} page={data.page} locale={siteLocale} />{" "} */}
+      <ProductPage data={data.product} locale={siteLocale} />{" "}
     </Wrapper>
   );
 }
